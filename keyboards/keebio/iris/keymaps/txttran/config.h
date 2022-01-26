0f3f2a214df0190a653f3f71ae2ad679693c16e3
Thomas Tran <ttran@ucdavis.edu>
2022-01-25 16:38:07 -0800

    Add animations

--- keyboards/keebio/iris/keymaps/txttran/config.h
+++ keyboards/keebio/iris/keymaps/txttran/config.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_SLEEP
+#define RGBLIGHT_ANIMATIONS
 
 #define TAPPING_TERM 175
 #define IGNORE_MOD_TAP_INTERRUPT
