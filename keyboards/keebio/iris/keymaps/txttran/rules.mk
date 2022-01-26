fc8e264d61cdf2b1830fd21f780905dae53a15e4
Thomas Tran <ttran@ucdavis.edu>
2022-01-25 16:37:39 -0800

    Enable underlights

--- keyboards/keebio/iris/keymaps/txttran/rules.mk
+++ keyboards/keebio/iris/keymaps/txttran/rules.mk
@@ -1,14 +1,4 @@
-BOOTMAGIC_ENABLE  = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE   = no       # Mouse keys(+4700)
-EXTRAKEY_ENABLE   = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE    = no         # Console for debug(+400)
+CONSOLE_ENABLE    = no        # Console for debug(+400)
 COMMAND_ENABLE    = no        # Commands for debug and configuration
-TAP_DANCE_ENABLE  = no
-RGBLIGHT_ENABLE   = yes
-AUDIO_ENABLE      = no
-NKRO_ENABLE       = yes
-BACKLIGHT_ENABLE  = no
-SPLIT_KEYBOARD    = yes
-SWAP_HANDS_ENABLE = no
+RGBLIGHT_ENABLE   = yes        # Enable keyboard RGB underglow
 SPACE_CADET_ENABLE = yes
-AUTO_SHIFT_ENABLE = no
